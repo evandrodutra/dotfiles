fed5225fbfe32708cd9d90c65f8111ce4b1718bf
Evandro Dutra <evandro.eld@gmail.com>
2017-10-09 15:51:40 +0200

    loading *.symlinks

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ export COMPUTER_NAME='marvin'
 # Ask for the administrator password upfront
 sudo -v
 
-setup_homebrew() {
 function setup_homebrew() {
   echo ""
   if test $(which brew); then
@@ -26,13 +25,45 @@ function setup_homebrew() {
   brew cleanup
 }
 
-setup_osx() {
-  sh $DOTFILES_ROOT/osx.sh
 function setup_osx() {
   sh $DOTFILES_ROOT/.macos
+}
+
+function link_file() {
+  if [ -e "$2" ]; then
+    if [ "$(readlink "$2")" = "$1" ]; then
+      echo "[INFO] Skipped, $1 already linked"
+      return 0
+    else
+      mv "$2" "$2.backup"
+      echo "[INFO] Moved $2 to $2.backup"
+    fi
+  fi
+  ln -sf "$1" "$2"
+  echo "[INFO] Linked $1 to $2"
+}
+
+function install_dotfiles() {
+  echo ""
+  if [ -d "$HOME/.dotfiles" ]; then
+    echo "[INFO] Skipped, $HOME/.dotfiles already linked"
+  else
+    echo "[INFO] Linking $DOTFILES_ROOT to $HOME/.dotfiles"
+    ln -s "$DOTFILES_ROOT" "$HOME/.dotfiles"
+  fi
+
+  set -e
+
+  echo "[INFO] Installing dotfiles"
+  find -H "$HOME/.dotfiles" -maxdepth 2 -name '*.symlink' -not -path '*.git*' |
+    while read -r src; do
+      dst="$HOME/.$(basename "${src%.*}")"
+      link_file "$src" "$dst"
+    done
 }
 
 if [ $OS == "Darwin" ]; then
   setup_homebrew()
   setup_osx()
+  install_dotfiles()
 fi
